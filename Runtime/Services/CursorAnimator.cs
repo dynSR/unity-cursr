58caa12a1d49d7da00f0ed39dd372c7484aa7b6a
Dylan Schumacker <Dylan.schumacker@outlook.fr>
2025-04-24 13:00:17 +0200

    refactored cursor animator, added animation type handle

--- Runtime/Services/CursorAnimator.cs
+++ Runtime/Services/CursorAnimator.cs
@@ -1,5 +1,6 @@
 ﻿using CursR.Runtime.Helpers;
-using CursR.Runtime.ScriptableObjects;
+using CursR.Runtime.ScriptableObjects.Configs.Cursor;
+using CursR.Runtime.ScriptableObjects.Configs.CursorAnimations;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityUtils;
@@ -7,10 +8,11 @@ using CursR.Runtime.Utils;
 
 namespace CursR.Runtime.Services {
     public class CursorAnimator {
-        private CursorConfig animatedCursorConfig;
-        private Texture2D[] animationFrames;
+        private CursorConfig cursor;
 
         private int animationFrameIndex;
+        private int frameShift = 1;
+
         private readonly LoopingTimer loopingTimer = new();
 
         public void OnEnable() => loopingTimer.AssignActionOnTimerFinished(PlayCursorAnimation);
@@ -19,34 +21,43 @@ namespace CursR.Runtime.Services {
         public void Update() => loopingTimer.Tick(Time.deltaTime);
 
         public void Init(CursorConfig cursorConfig) {
-            animatedCursorConfig = cursorConfig;
-            CursorAnimationConfig cursorAnimationConfig = animatedCursorConfig.AnimationConfig;
-            SetAnimationFrames(cursorAnimationConfig.GetAnimationFramesByCursorSize().ToArray());
-            loopingTimer.Reset(cursorAnimationConfig.GetAnimationSpeed());
+            cursor = cursorConfig;
+            CursorAnimationConfig animation = cursor.GetAnimation();
+
+            if (!loopingTimer.IsStopped()) loopingTimer.Reset(animation.Speed);
+            else loopingTimer.Start(animation.Speed);
         }
 
-        private void SetAnimationFrames(Texture2D[] newFrames) {
-            Assert.IsNotNull(newFrames, "No cursor animation frames provided");
-            animationFrames = newFrames;
+        public void Reset() {
+            cursor = null;
+            loopingTimer.Stop();
         }
 
         private void PlayCursorAnimation() =>
-            CursorUtils.SetCursorAppearance(GetCurrentAnimationFrame(animationFrames), animatedCursorConfig.IsCentered);
+            CursorUtils.SetCursorAppearance(GetCurrentAnimationFrame(cursor.GetAnimation()), cursor.IsCentered);
 
-        private Texture2D GetCurrentAnimationFrame(Texture2D[] frames) {
+        private Texture2D GetCurrentAnimationFrame(CursorAnimationConfig animation) {
+            var frames = animation.Frames.ToArray();
             Assert.IsFalse(frames.IsNullOrEmpty(), "No cursor animation frames provided");
 
             Texture2D currentFrame = frames[animationFrameIndex];
             Assert.IsNotNull(currentFrame, "Current cursor animation frame is not defined");
-            IncrementFrameIndex(frames);
+
+            SetFrameIndex(frames, animation.IsLooping(), ref animationFrameIndex);
             return currentFrame;
         }
 
-        private void IncrementFrameIndex(Texture2D[] frames) {
-            animationFrameIndex = (animationFrameIndex + 1) % frames.Length;
-            Assert.IsFalse(animationFrameIndex < 0, "Error: frame index, must be greater than 0");
-            Assert.IsFalse(animationFrameIndex >= frames.Length,
-                "Error: frame index, must be lower or equal to: " + frames.Length);
+        private void SetFrameIndex(Texture2D[] frames, bool isLooping, ref int index) {
+            if (isLooping) index = (index + 1) % frames.Length;
+            else {
+                if (IsAnimationCycleDone(index, frames.Length - 1)) frameShift = index == 0 ? 1 : -1;
+                index = (index + frameShift) % frames.Length;
+            }
+
+            Assert.IsFalse(index < 0 && index >= frames.Length,
+                "Error: frame index, must be between 0 and " + frames.Length);
         }
+
+        private static bool IsAnimationCycleDone(int index, int maxFrames) => index == 0 || index == maxFrames;
     }
 }
