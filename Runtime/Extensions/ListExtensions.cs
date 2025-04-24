22a9dc140bbc9842c3787b8558a3408f6f5d02f7
Dylan Schumacker <Dylan.schumacker@outlook.fr>
2025-04-24 12:53:24 +0200

    added list extensions

---
+++ Runtime/Extensions/ListExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CursR.Runtime.Extensions {
+    public static class ListExtensions {
+        public static bool IsEmpty<T>(this List<T> source) => source.Count == 0;
+
+        public static void RemoveWhere<T>(this List<T> source, Predicate<T> predicate) {
+            if (source.IsEmpty()) return;
+            for (int i = source.Count - 1; i >= 0; i--)
+                if (predicate(source[i])) {
+                    source.RemoveAt(i);
+                }
+        }
+    }
+}
