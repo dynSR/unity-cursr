59e5176da811c1dc2058b36e4ee244800fdb8c56
Dylan Schumacker <Dylan.schumacker@outlook.fr>
2025-04-24 12:55:34 +0200

    removed old implementation related files

--- Runtime/Interfaces/ICursorProperty.cs
+++
@@ -1,9 +0,0 @@
-﻿using CursR.Runtime.Enums;
-
-namespace CursR.Runtime.Interfaces {
-    public interface ICursorProperty<out T> {
-        CursorSize AssociatedCursorSize { get; }
-        T Value { get; }
-        T Get() => Value;
-    }
-}
